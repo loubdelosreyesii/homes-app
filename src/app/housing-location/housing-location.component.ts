0046dc2e47da25e5bbfc17104dff16f5eb236f60
Lou de los Reyes II <loubdelosreyesii@gmail.com>
2024-07-07 15:45:04 +0800

    Structure of the Housing-Location

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrls: ['./housing-location.component.css']
+  template: ` 
+  <section class="listing">
+    <img class="listing-photo">
+    <h2 class="listing-heading"></h2>
+    <p class="listing-location"></p>
+  </section> `,
+  styleUrls: ['./housing-location.component.css'],
 })
-export class HousingLocationComponent {
-
-}
+export class HousingLocationComponent {}
